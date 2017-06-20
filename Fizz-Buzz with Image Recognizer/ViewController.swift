d261272cd0edc3dfa5d173b675bf78af65159969
Michael Kane <mediocrecomedy@gmail.com>
2017-06-19 23:46:23 -0700

    begin writing logic for view controller

--- Fizz-Buzz with Image Recognizer/ViewController.swift
+++ Fizz-Buzz with Image Recognizer/ViewController.swift
@@ -11,21 +11,35 @@ import CoreML
 
 class ViewController: UIViewController {
     
+    var lastPoint = CGPoint.zero
+    var swiped = false
+    var opacity: CGFloat = 1.0
     
     
+    @IBOutlet weak var mainImageView: UIImageView!
+    @IBOutlet weak var tempImageView: UIImageView!
     
     
-
+    @IBAction func resetButtonPressed(_ sender: Any) {
+        // Clears all curent user inputs
+    }
+    
+    @IBAction func submitNumberButtonPressed(_ sender: Any) {
+        // submits user input for image classification
+    }
+    
+    @IBAction func doneButtonPressed(_ sender: Any) {
+        // converts all user input to a number and plays fizzbuzz
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let test = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0]
+        let helper = clfHelper()
+        print("The output is a \(helper.readOutput(outputFromModel: test))!")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
