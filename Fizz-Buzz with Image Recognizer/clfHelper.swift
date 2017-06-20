6826777767a9092e7e4ca1265c775cc192372877
Michael Kane <mediocrecomedy@gmail.com>
2017-06-19 23:46:47 -0700

    initial commit, create function for translating outputs from one-hot encoding

---
+++ Fizz-Buzz with Image Recognizer/clfHelper.swift
@@ -0,0 +1,29 @@
+//
+//  clf_helper.swift
+//  Fizz-Buzz with Image Recognizer
+//
+//  Created by Michael Kane on 6/19/17.
+//  Copyright © 2017 Mike Kane. All rights reserved.
+//
+
+import Foundation
+
+class clfHelper {
+    // Singleton helper class designed to abstract away all of the image classification tasks.
+    
+    
+    
+    func readOutput(outputFromModel: Array<Double>) -> String {
+        // Helper function.  Takes the one-hot encoded output from the model
+        // and returns a string representation of the number it was classified as.
+        var classification: Int!
+        for i in outputFromModel {
+            if i == 1 {
+                classification = outputFromModel.index(of: i)! + 1
+            }
+        }
+        return "\(classification!)"
+    }
+        
+        
+}
